c2d1f0333493e2c5c2dfc9b8518e9e417af0eb39
yujin9619 <yujin9588@gmail.com>
2021-09-30 10:41:38 +0900

    검진기록 xml 추가

---
+++ app/src/main/java/com/yujin/forpaw/AddCheckUpActivity.kt
@@ -0,0 +1,11 @@
+package com.yujin.forpaw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCheckUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_check_up)
+    }
+}
---
+++ app/src/main/java/com/yujin/forpaw/CheckUpActivity.kt
@@ -0,0 +1,34 @@
+package com.yujin.forpaw
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.yujin.forpaw.databinding.ActivityCheckUpBinding
+
+class CheckUpActivity : BaseActivity() {
+
+    lateinit var binding : ActivityCheckUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_check_up)
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+        binding.addBtn.setOnClickListener {
+
+           val addIntent = Intent(mContext, AddCheckUpActivity::class.java)
+            startActivity(addIntent)
+        }
+
+    }
+
+    override fun setValues() {
+
+    }
+}
