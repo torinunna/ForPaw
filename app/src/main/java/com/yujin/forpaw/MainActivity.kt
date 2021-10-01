b201c10c7b3a54aa1d9fda24f644bb2c89ce3320
yujin9619 <yujin9588@gmail.com>
2021-10-01 11:24:30 +0900

    button text color 변경 friendlistview setOnItemClickListener 추가

--- app/src/main/java/com/yujin/forpaw/MainActivity.kt
+++ app/src/main/java/com/yujin/forpaw/MainActivity.kt
@@ -2,45 +2,65 @@ package com.yujin.forpaw
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ListView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.adapters.AdapterViewBindingAdapter
 import com.yujin.forpaw.adapters.FriendAdapter
 import com.yujin.forpaw.databinding.ActivityMainBinding
+import com.yujin.forpaw.databinding.FriendListItemBinding
 import com.yujin.forpaw.datas.FriendData
 
 class MainActivity : BaseActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     val mFriendList = ArrayList<FriendData>()
 
-    lateinit var mAdapter : FriendAdapter
+    lateinit var mAdapter: FriendAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         setupEvents()
         setValues()
 
-        mFriendList.add( FriendData("권토리", 2016 ))
-        mFriendList.add( FriendData("권베베", 2019 ))
+        mFriendList.add(FriendData("권토리", 2016))
+        mFriendList.add(FriendData("권베베", 2019))
 
-        mAdapter = FriendAdapter( this, R.layout.friend_list_item, mFriendList )
+        mAdapter = FriendAdapter(this, R.layout.friend_list_item, mFriendList)
 
         binding.friendListView.adapter = mAdapter
+
+
+        binding.friendListView.setOnItemClickListener { adapterView, view, i, l ->
+
+            val friendMainIntent = Intent(mContext, FriendMainActivity::class.java)
+            startActivity((friendMainIntent))
+        }
+
+
     }
 
 
     override fun setupEvents() {
 
+
         binding.addFriendBtn.setOnClickListener {
             val addfriendIntent = Intent(mContext, AddFriendActivity::class.java)
-            startActivity(addfriendIntent)
+            startActivity(addfriendIntent) }
 
+
+
+
+
+
+    }
+
+        override fun setValues() {
         }
-    }
-
-    override fun setValues() {
-    }
 
 }
+
